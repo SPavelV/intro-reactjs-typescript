bd6bfe4220522f4d234e299d22505d658e4ee1ca
Solovev Pavel <p.solovev@elewise.com>
2020-03-28 13:33:42 +0400

    add todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React, {useState} from 'react'
 import { Navbar } from './components/Navbar'
 import {TodoForm} from './components/TodoForm'
+import {TodoList} from './components/TodoList'
 
 const App: React.FunctionComponent = () => {
   const [todos, setTodos] = useState([]);
 
   const addHandler = (title: string) => {
-    console.log('Add New Todo :', title);
+    const newTodo = {
+      title: title,
+      id: Date.now(),
+      complete: false 
+    }
+    setTodos([newTodo, ...todos])
   }
-  
+
   return <>
     <Navbar/>
     <div className="container">
       <TodoForm onAdd ={addHandler}/>
+      <TodoList todos={todos}/>
     </div>
   </>
 }
