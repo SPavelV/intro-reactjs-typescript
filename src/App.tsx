906b69aa68d105f83fdcb5b75866bc846a029613
Solovev Pavel <p.solovev@elewise.com>
2020-03-28 16:01:47 +0400

    Update todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
-import { Navbar } from './components/Navbar'
-import {TodoForm} from './components/TodoForm'
-import {TodoList} from './components/TodoList'
-import { ITodo } from './interfaces'
+import React, { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
 
 const App: React.FunctionComponent = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -11,19 +11,39 @@ const App: React.FunctionComponent = () => {
     const newTodo: ITodo = {
       title: title,
       id: Date.now(),
-      completed: false 
-    }
-    // setTodos([newTodo, ...todos])
-    setTodos(prev => [newTodo, ...todos])
-  }
+      completed: false
+    };
+    setTodos(prev => [newTodo, ...todos]);
+  };
 
-  return <>
-    <Navbar/>
-    <div className="container">
-      <TodoForm onAdd ={addHandler}/>
-      <TodoList todos={todos}/>
-    </div>
-  </>
-}
+  const toggleHandler = (id: number) => {
+    setTodos(prev =>
+      prev.map(todo => {
+        if(todo.id === id) {
+          return {...todo, completed: !todo.completed}
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <TodoForm onAdd={addHandler} />
+        <TodoList
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+        />
+      </div>
+    </>
+  );
+};
 
 export default App;
